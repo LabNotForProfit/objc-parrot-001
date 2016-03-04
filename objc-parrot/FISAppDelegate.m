7810f37e70763cfa42c8c7a3d3efc48bd2359099
JeffChiu <jchiu@KCC.local>
2016-03-03 16:08:42 -0800

    solution

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -12,6 +12,24 @@
      *  Write your code here!
      
      */
+    //Code-Along I: "Squawk!"
+    
+    //A
+    NSLog(@"Squawk!");
+    NSLog(@"%@", @"Squawk!");
+    NSLog(@"%@!", @"Squawk");
+    NSLog(@"%@!", [@"Squawk" uppercaseString]);
+    
+    //B. NSString Variable
+    NSString *squawk = @"squawk";
+    //important to remember that we are creating variable called squawk
+    //and set it equal to a string literal @"squawk" 
+    NSLog(@"%@", squawk);
+    squawk = [squawk uppercaseString];
+    NSLog(@"%@", squawk);
+    squawk = [squawk stringByAppendingString:@"!"];
+    NSLog(@"%@", squawk);
+
     
     // Do not alter
     return YES;  //
